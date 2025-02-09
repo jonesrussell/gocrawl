b91a96aa3733990d8450d32f7808f011cb6d7142
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-02-09 14:06:45 -0500

    Refactor collector initialization into separate package
    - Move collector initialization logic to new `collector` package
    - Remove inline collector configuration methods from crawler
    - Simplify Crawler struct initialization by using new collector package
    - Update import statements and function calls accordingly

---
+++ internal/collector/collector.go
@@ -0,0 +1,76 @@
+package collector
+
+import (
+	"fmt"
+	"net/url"
+	"time"
+
+	"github.com/jonesrussell/gocrawl/internal/logger"
+
+	"github.com/gocolly/colly/v2"
+)
+
+// New creates and returns a new colly collector
+func New(baseURL string, maxDepth int, rateLimit time.Duration, debugger *logger.CustomDebugger) (*colly.Collector, error) {
+	parsedURL, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse baseURL: %w", err)
+	}
+	allowedDomain := parsedURL.Hostname()
+
+	collector := colly.NewCollector(
+		colly.Async(true),
+		colly.MaxDepth(maxDepth),
+		colly.Debugger(debugger),
+		colly.AllowedDomains(
+			allowedDomain,
+			"http://"+allowedDomain,
+			"https://"+allowedDomain,
+		),
+	)
+
+	collector.Limit(&colly.LimitRule{
+		DomainGlob:  "*",
+		Parallelism: 2,
+		Delay:       rateLimit,
+	})
+
+	return collector, nil
+}
+
+// ConfigureLogging sets up logging for the collector
+func ConfigureLogging(collector *colly.Collector, log *logger.CustomLogger) {
+	collector.OnRequest(func(r *colly.Request) {
+		startTime := time.Now()
+		logRequest(log, "Requesting URL", r, startTime)
+
+		defer func() {
+			duration := time.Since(startTime)
+			logRequest(log, "Request completed", r, duration)
+		}()
+	})
+
+	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Request.AbsoluteURL(e.Attr("href"))
+		if err := e.Request.Visit(link); err != nil {
+			logVisitError(log, link, err)
+		}
+	})
+}
+
+func logRequest(log *logger.CustomLogger, message string, r *colly.Request, data interface{}) {
+	log.Info(message, log.Field("url", r.URL.String()), log.Field("request_id", r.ID), log.Field("data", data))
+}
+
+func logVisitError(log *logger.CustomLogger, link string, err error) {
+	switch err.Error() {
+	case "URL already visited":
+		log.Info("URL already visited", log.Field("link", link))
+	case "Forbidden domain", "Missing URL":
+		log.Info(err.Error(), log.Field("link", link))
+	case "Max depth limit reached":
+		log.Warn("Max depth limit reached", log.Field("link", link))
+	default:
+		log.Error("Error visiting link", log.Field("link", link), log.Field("error", err))
+	}
+}
--- internal/crawler/crawler.go
+++ internal/crawler/crawler.go
@@ -5,10 +5,10 @@ import (
 	"crypto/md5"
 	"encoding/hex"
 	"fmt"
-	"net/url"
 	"time"
 
 	"github.com/gocolly/colly/v2"
+	"github.com/jonesrussell/gocrawl/internal/collector"
 	"github.com/jonesrussell/gocrawl/internal/config"
 	"github.com/jonesrussell/gocrawl/internal/logger"
 	"github.com/jonesrussell/gocrawl/internal/storage"
@@ -41,20 +41,20 @@ func NewCrawler(baseURL string, maxDepth int, rateLimit time.Duration, debugger 
 	log.Info("Successfully connected to Elasticsearch")
 
 	// Initialize collector
-	collector, err := initializeCollector(baseURL, maxDepth, rateLimit, debugger)
+	collectorInstance, err := collector.New(baseURL, maxDepth, rateLimit, debugger)
 	if err != nil {
 		return nil, fmt.Errorf("failed to initialize collector: %w", err)
 	}
 
 	// Configure logging for the collector
-	configureCollectorLogging(collector, log)
+	collector.ConfigureLogging(collectorInstance, log)
 
 	return &Crawler{
 		BaseURL:   baseURL,
 		Storage:   storage,
 		MaxDepth:  maxDepth,
 		RateLimit: rateLimit,
-		Collector: collector,
+		Collector: collectorInstance,
 		Logger:    log,
 		Done:      make(chan bool),
 		IndexName: cfg.IndexName,
@@ -77,71 +77,6 @@ func initializeStorage(cfg *config.Config, log *logger.CustomLogger) (*storage.S
 	}
 
 	return storage, nil
-}
-
-func initializeCollector(baseURL string, maxDepth int, rateLimit time.Duration, debugger *logger.CustomDebugger) (*colly.Collector, error) {
-	parsedURL, err := url.Parse(baseURL)
-	if err != nil {
-		return nil, fmt.Errorf("failed to parse baseURL: %w", err)
-	}
-	allowedDomain := parsedURL.Hostname()
-
-	collector := colly.NewCollector(
-		colly.Async(true),
-		colly.MaxDepth(maxDepth),
-		colly.Debugger(debugger),
-		// Set allowed domains directly
-		colly.AllowedDomains(
-			allowedDomain,
-			"http://"+allowedDomain,
-			"https://"+allowedDomain,
-		),
-	)
-
-	collector.Limit(&colly.LimitRule{
-		DomainGlob:  "*",
-		Parallelism: 2,
-		Delay:       rateLimit,
-	})
-
-	return collector, nil
-}
-
-func configureCollectorLogging(collector *colly.Collector, log *logger.CustomLogger) {
-	collector.OnRequest(func(r *colly.Request) {
-		startTime := time.Now()
-		logRequest(log, "Requesting URL", r, startTime)
-
-		defer func() {
-			duration := time.Since(startTime)
-			logRequest(log, "Request completed", r, duration)
-		}()
-	})
-
-	collector.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Request.AbsoluteURL(e.Attr("href"))
-		if err := e.Request.Visit(link); err != nil {
-			logVisitError(log, link, err)
-		}
-	})
-}
-
-func logRequest(log *logger.CustomLogger, message string, r *colly.Request, data interface{}) {
-	log.Info(message, log.Field("url", r.URL.String()), log.Field("request_id", r.ID), log.Field("data", data))
-}
-
-func logVisitError(log *logger.CustomLogger, link string, err error) {
-	switch err.Error() {
-	case "URL already visited":
-		log.Info("URL already visited", log.Field("link", link))
-	case "Forbidden domain", "Missing URL":
-		log.Info(err.Error(), log.Field("link", link))
-	case "Max depth limit reached":
-		// Log a simple message without stack trace
-		log.Warn("Max depth limit reached", log.Field("link", link))
-	default:
-		log.Error("Error visiting link", log.Field("link", link), log.Field("error", err))
-	}
 }
 
 // Start method to begin crawling
