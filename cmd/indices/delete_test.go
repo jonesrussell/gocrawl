6ed108be889aa3dbd28c427aec5a5fe370e10bfe
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2025-04-03 06:30:53 -0400

    fix(tests): Remove shadowing in TestDeleteCommand
    - Eliminated shadowing of the test case variable in the TestDeleteCommand function by removing the redundant declaration.
    - This change enhances code clarity and aligns with best practices for maintaining clean and understandable test code.

--- cmd/indices/delete_test.go
+++ cmd/indices/delete_test.go
@@ -181,7 +181,6 @@ func TestDeleteCommand(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			// Create mocks
 			mockStore := &test.MockStorage{}
